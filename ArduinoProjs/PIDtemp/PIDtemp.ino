2bdf6b4bbdacba1bb2d9deee5fb386fff837696c
HWANG SEEHYUN <simon5678@naver.com>
2023-10-23 23:07:36 +0900

    Fixed PID temp controller

--- ArduinoProjs/PIDtemp/PIDtemp.ino
+++ ArduinoProjs/PIDtemp/PIDtemp.ino
@@ -1,14 +1,13 @@
 #include <PID_v1.h>
 
-#define DIAL_START_PIN 4
-#define TEMP_PIN A0
-#define PWM_PIN 3
+#define DIAL_START_PIN 4 //다이얼 디지털핀 시작 핀
+#define TEMP_PIN A0 //온도센서핀
+#define PWM_PIN 3 // 릴레이 출력핀
 
-//Define Variables we'll be connecting to
 double Setpoint, Input, Output;
 
-//Specify the links and initial tuning parameters
 double Kp=2, Ki=5, Kd=1;
+
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup()
@@ -26,10 +25,10 @@ void loop()
 {
   for(int i=0;i<12;i++) {
     if(digitalRead(DIAL_START_PIN+i)==LOW) {
-      Setpoint = 512 + 10 * i;
+      Setpoint = 30 + 5 * i; //다이얼 온도 설정. 기본 설정은 30도부터 다이얼 1칸당 10도씩 증가
     }
   }
-  Input = analogRead(TEMP_PIN);
+  Input = (analogRead(TEMP_PIN) * 5.0 / 1024.0 - 0.5)*100; //센서 입력 섭씨 온도
   myPID.Compute();
   analogWrite(PWM_PIN, Output);
 }
